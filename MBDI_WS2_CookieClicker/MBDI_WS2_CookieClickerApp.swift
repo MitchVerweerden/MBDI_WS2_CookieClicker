dea834a8eb927d6d4ececd9528ba94314593de11
Mitchell Verweerden <m.verweerden@student.avans.nl>
2022-03-16 12:36:11 +0100

    Initial Commit

---
+++ MBDI_WS2_CookieClicker/MBDI_WS2_CookieClickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MBDI_WS2_CookieClickerApp.swift
+//  MBDI_WS2_CookieClicker
+//
+//  Created by Mitchell Verweerden on 16/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct MBDI_WS2_CookieClickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
